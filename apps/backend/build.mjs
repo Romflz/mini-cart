ad0da7ea0b0b6185146c13500b17473dda2bf9d6
romflz <electronicmusicplease@gmail.com>
2025-12-09 19:13:35 +0100

    fix: build es bundler for init error fix for structure change

--- apps/backend/build.mjs
+++ apps/backend/build.mjs
@@ -1,7 +1,7 @@
 import * as esbuild from 'esbuild'
 
 await esbuild.build({
-  entryPoints: ['src/server.ts', 'src/db/seed.ts'],
+  entryPoints: ['src/server.ts', 'src/db/init/index.ts'],
   bundle: true,
   platform: 'node',
   format: 'esm',
